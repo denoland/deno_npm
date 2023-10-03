6f350bd2196dbb936f2b7f7eb8a714255a8d34b3
Rano | Ranadeep <ranadip.bswas@gmail.com>
2023-10-03 18:52:50 +0300

    fix: resolve a version requirement to the latest dist tag if it matches (#37)

--- src/resolution/common.rs
+++ src/resolution/common.rs
@@ -83,10 +83,18 @@ impl NpmVersionResolver {
     if let Some(tag) = version_req.tag() {
       self.tag_to_version_info(info, tag)
       // When the version is *, if there is a latest tag, use it directly.
+      // When the latest tag satisfies the version requirement, use it directly.
+      // https://github.com/npm/npm-pick-manifest/blob/67508da8e21f7317e3159765006da0d6a0a61f84/lib/index.js#L125
       // No need to care about @types/node here, because it'll be handled specially below.
-    } else if *version_req == *WILDCARD_VERSION_REQ
-      && info.dist_tags.contains_key("latest")
+    } else if info.dist_tags.contains_key("latest")
       && info.name != "@types/node"
+      && (*version_req == *WILDCARD_VERSION_REQ
+        || info
+          .dist_tags
+          .get("latest")
+          .map(|version| self.version_req_satisfies(version_req, version, info))
+          .transpose()?
+          .unwrap_or_default())
     {
       self.tag_to_version_info(info, "latest")
     } else {
@@ -332,5 +340,65 @@ mod test {
       &package_info,
     );
     assert_eq!(result.unwrap().version.to_string(), "1.0.0-rc.1");
+  }
+
+  #[test]
+  fn test_latest_tag_version_req() {
+    let package_req = PackageReq::from_str("some-pkg@^0.1.0-alpha.2").unwrap();
+    let package_info = NpmPackageInfo {
+      name: "some-pkg".to_string(),
+      versions: HashMap::from([
+        (
+          Version::parse_from_npm("0.1.0-alpha.1").unwrap(),
+          NpmPackageVersionInfo {
+            version: Version::parse_from_npm("0.1.0-alpha.1").unwrap(),
+            ..Default::default()
+          },
+        ),
+        (
+          Version::parse_from_npm("0.1.0-alpha.2").unwrap(),
+          NpmPackageVersionInfo {
+            version: Version::parse_from_npm("0.1.0-alpha.2").unwrap(),
+            ..Default::default()
+          },
+        ),
+        (
+          Version::parse_from_npm("0.1.0-beta.1").unwrap(),
+          NpmPackageVersionInfo {
+            version: Version::parse_from_npm("0.1.0-beta.1").unwrap(),
+            ..Default::default()
+          },
+        ),
+        (
+          Version::parse_from_npm("0.1.0-beta.2").unwrap(),
+          NpmPackageVersionInfo {
+            version: Version::parse_from_npm("0.1.0-beta.2").unwrap(),
+            ..Default::default()
+          },
+        ),
+      ]),
+      dist_tags: HashMap::from([
+        (
+          "latest".to_string(),
+          Version::parse_from_npm("0.1.0-alpha.2").unwrap(),
+        ),
+        (
+          "dev".to_string(),
+          Version::parse_from_npm("0.1.0-beta.2").unwrap(),
+        ),
+      ]),
+    };
+    let resolver = NpmVersionResolver {
+      types_node_version_req: None,
+    };
+    let result = resolver.get_resolved_package_version_and_info(
+      &package_req.version_req,
+      &package_info,
+    );
+    assert_eq!(
+      result.as_ref().unwrap().version.to_string(),
+      "0.1.0-alpha.2"
+    );
+    assert_ne!(result.as_ref().unwrap().version.to_string(), "0.1.0-beta.2");
   }
 }
