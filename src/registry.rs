9ba903bee8f93b64576d4661cc2863c4cd8eaa31
David Sherret <dsherret@gmail.com>
2025-09-12 18:06:06 -0700

    let us make this more versatile

--- src/registry.rs
+++ src/registry.rs
@@ -122,13 +122,6 @@ impl Ord for NpmDependencyEntry {
   }
 }
 
-#[derive(Debug, Default, Serialize, Deserialize, Clone, PartialEq, Eq)]
-pub struct NpmPackageVersionInfoDirectories {
-  #[serde(deserialize_with = "deserializers::string")]
-  pub bin: Option<String>,
-}
-
-
 #[derive(Debug, Default, Deserialize, Serialize, Clone, PartialEq, Eq)]
 pub struct NpmPeerDependencyMeta {
   #[serde(default)]
@@ -177,8 +170,9 @@ pub struct NpmPackageVersionInfo {
   #[serde(default, skip_serializing_if = "Vec::is_empty")]
   #[serde(deserialize_with = "deserializers::vector")]
   pub cpu: Vec<SmallStackString>,
-  #[serde(default, skip_serializing_if = "Option::is_none")]
-  pub directories: Option<NpmPackageVersionInfoDirectories>,
+  #[serde(default, skip_serializing_if = "HashMap::is_empty")]
+  #[serde(deserialize_with = "deserializers::hashmap")]
+  pub directories: HashMap<SmallStackString, String>,
   #[serde(default, skip_serializing_if = "HashMap::is_empty")]
   #[serde(deserialize_with = "deserializers::hashmap")]
   pub scripts: HashMap<SmallStackString, String>,
@@ -1146,7 +1140,8 @@ mod test {
         "peerDependenciesMeta": [],
         "os": {},
         "cpu": {},
-        "scripts": []
+        "scripts": [],
+        "directories": []
       }"#,
     );
 
